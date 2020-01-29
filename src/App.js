78a41c4c5f1c15ad8be32ce6f8af941d65b35b5d
emmanuel <kipronofb@gmail.com>
2020-01-29 20:35:44 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import logo from "./assets/images/logo.svg";
 import {Typography, } from "@material-ui/core"
+import clsx from "clsx"
 import {WhatsApp, Facebook, Twitter, Instagram} from "@material-ui/icons"
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      flip: true
+    }
+  }
+
+  click=(event)=>{
+    event.preventDefault()
+    this.setState({flip: !this.state.flip})
+  }
   render(){
+    
     return (
       <div className="App">
           <AppBar className="top-nav">
@@ -21,7 +34,16 @@ class App extends React.Component {
           </AppBar>
           <div className="cover">
           <div className="under-construction-wrapper">
-            <img alt="" src={logo} />
+            <div 
+              className={clsx("logo", {
+                "flip": this.state.flip
+              })} 
+              onClick={this.click}
+            >
+                <img alt="" src={logo} 
+            />
+
+            </div>
             <Typography>Website coming soon!</Typography> 
             <div className="contacts">
               <IconButton>
