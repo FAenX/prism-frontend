2ecd1f080f110daaeed3a20e745365ac6743b969
emmanuel <kipronofb@gmail.com>
2020-01-29 19:46:41 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import AppBar from "@material-ui/core/AppBar"
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from "./assets/images/logo.svg";
+import {Typography, } from "@material-ui/core"
+import {WhatsApp, Facebook, Twitter, Instagram} from "@material-ui/icons"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+          <AppBar className="top-nav">
+            <div>
+              <IconButton>
+                <MenuIcon />
+              </IconButton>
+            </div>
+         
+          </AppBar>
+          <div className="cover">
+          <div className="under-construction-wrapper">
+            <img alt="" src={logo} />
+            <Typography>Website coming soon!</Typography> 
+            <div className="contacts">
+              <IconButton>
+                <Facebook />
+              </IconButton>
+              <IconButton>
+                <WhatsApp />
+              </IconButton>
+              
+              <IconButton>
+                <Twitter />
+              </IconButton>
+              <IconButton>
+                <Instagram />
+              </IconButton>
+             
+            </div>
+          </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
